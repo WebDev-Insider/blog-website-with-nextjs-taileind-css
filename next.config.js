0b6b62d9a56c9ab5761d8caf6c0b2439e2aed917
WebDevInsider <henrydustin95@gmail.com>
2023-11-20 07:58:49 -1100

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
+// next.config.js
+module.exports = {
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NODE_ENV === 'production' ? 'https://production-api.com' : 'http://localhost:3000',
+  },
+};
 
 module.exports = nextConfig
